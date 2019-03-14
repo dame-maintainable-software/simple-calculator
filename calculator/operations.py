b609347a77469ce5db036178b9f86a2da1bfc06a
Markus Goetz <markus.goetz@kit.edu>
2019-03-14 15:23:19 +0100

    Implemented remaining operations and modulo

--- calculator/operations.py
+++ calculator/operations.py
@@ -5,14 +5,18 @@ def add(lhs, rhs):
 
 def subtract(lhs, rhs):
     """subtract the two numbers lhs and rhs. In this implementation we use our own add function"""
-    return None
+    return lhs-rhs
 
 
 def multiply(lhs, rhs):
     """multiply the two numbers lhs and rhs"""
-    return None
+    return lhs*rhs
 
 
 def devide(lhs, rhs):
     """devide the numbers lhs by rhs. In this implementation we use our own multiply function"""
     return lhs/rhs
+
+def modulo(lhs, rhs):
+    """lhs module rhs"""
+    return lhs%rhs
