931b74628c303432f5a433ca6c1480d30781b367
Marie Weiel <Marie@Maries-MacBook-Pro.local>
2019-03-14 15:48:15 +0100

    added conflict in subtraction function

--- calculator/operations.py
+++ calculator/operations.py
@@ -5,7 +5,7 @@ def add(lhs, rhs):
 
 def subtract(lhs, rhs):
     """subtract the two numbers lhs and rhs. In this implementation we use our own add function"""
-    return None
+    return lhs
 
 
 def multiply(lhs, rhs):
