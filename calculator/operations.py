72f71df4fb35c37a390866a93b0f980106866a0e
stellalessandra <a.stella@fz-juelich.de>
2019-03-14 16:47:37 +0100

    implemented operations

--- calculator/operations.py
+++ calculator/operations.py
@@ -5,14 +5,14 @@ def add(lhs, rhs):
 
 def subtract(lhs, rhs):
     """subtract the two numbers lhs and rhs. In this implementation we use our own add function"""
-    return None
+    return add(lhs, -rhs)
 
 
 def multiply(lhs, rhs):
     """multiply the two numbers lhs and rhs"""
-    return None
+    return lhs*rhs
 
 
 def divide(lhs, rhs):
     """divide the numbers lhs by rhs. In this implementation we use our own multiply function"""
-    return lhs/rhs
+    return multiply(lhs, 1/rhs)
