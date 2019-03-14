ffafd6f0fa17ebb626371a33f5727e3bafa59e05
Arthur Voronin <arthur.voronin@kit.edu>
2019-03-14 15:54:57 +0100

    added multiply operation

--- calculator/operations.py
+++ calculator/operations.py
@@ -1,6 +1,6 @@
 def add(lhs, rhs):
     """add the two numbers lhs and rhs"""
-    return lhs+rhs
+    return lhs + rhs
 
 
 def subtract(lhs, rhs):
@@ -10,9 +10,9 @@ def subtract(lhs, rhs):
 
 def multiply(lhs, rhs):
     """multiply the two numbers lhs and rhs"""
-    return None
+    return lhs * rhs
 
 
 def devide(lhs, rhs):
     """devide the numbers lhs by rhs. In this implementation we use our own multiply function"""
-    return lhs/rhs
+    return lhs / rhs
